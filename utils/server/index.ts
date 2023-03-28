c8bbf115a7b8388737b6ffb8c2814bcdc5f6fa33
Jordan Burgess <jordan@humanloop.com>
2023-03-28 11:22:31 +0100

    Add breaking hl call in edge runtime

--- utils/server/index.ts
+++ utils/server/index.ts
@@ -6,6 +6,8 @@ import {
   ReconnectInterval,
 } from 'eventsource-parser';
 import { OPENAI_API_HOST } from '../app/const';
+import { Humanloop } from 'humanloop';
+import { Configuration, OpenAIApi } from 'openai';
 
 export const OpenAIStream = async (
   model: OpenAIModel,
@@ -33,6 +35,51 @@ export const OpenAIStream = async (
       stream: true,
     }),
   });
+
+  // const config = new Configuration({
+  //   apiKey: key ? key : process.env.OPENAI_API_KEY,
+  // });
+
+  // const oai = new OpenAIApi(config);
+  // const oaiResponse = await oai.createCompletion({
+  //   model: 'text-davinci-003',
+  //   prompt:
+  //     `System: ${systemPrompt}\n` +
+  //     messages.map((m) => `${m.role}: ${m.content}`).join('\n'),
+  //   max_tokens: 1000,
+  //   temperature: 1,
+  //   // stream: true,
+  // });
+  // console.log(oaiResponse);
+
+  // Using this here in edge runtime ends with a [TypeError: adapter is not a function]
+  const hl = new Humanloop({
+    apiKey: process.env.HUMANLOOP_API_KEY,
+    // Would like this
+    // openaiApiKey: process.env.OPENAI_API_KEY,
+    // anthropicApiKey: process.env.ANTHROPIC_API_KEY,
+  });
+
+  const hlResponse = await hl.chat({
+    project: 'ts-sdk-test',
+    messages: [
+      {
+        role: 'system',
+        content: systemPrompt,
+      },
+      ...messages,
+    ],
+    model_config: {
+      model: model.id,
+      max_tokens: 1000,
+      temperature: 1,
+    },
+    // This would now be optional override of the other keys
+    provider_api_keys: {
+      openai: key ? key : process.env.OPENAI_API_KEY,
+    },
+  });
+  console.log(hlResponse);
 
   if (res.status !== 200) {
     const statusText = res.statusText;
